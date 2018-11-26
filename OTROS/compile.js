d82622585de90b369de4ff7be88e493ae7188091
Javier Eguren <javier.emateo@gmail.com>
2018-11-26 18:52:19 +0100

    ok

--- OTROS/compile.js
+++ OTROS/compile.js
@@ -1,7 +1,7 @@
 //LG probando es muy majo y se entera de todo Git
 
 
-const path = require('path2');
+const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 const buildPath = path.resolve(__dirname, 'build');
